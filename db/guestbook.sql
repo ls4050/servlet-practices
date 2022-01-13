d7e8dd481b04ec2ce1b08df1ff883c630a9381c7
Wonseok <ls4050@naver.com>
2022-01-13 12:31:10 +0900

    guestbook01 #1

---
+++ db/guestbook.sql
@@ -0,0 +1,14 @@
+SELECT * FROM webdb.guestbook;
+
+show tables;
+desc guestbook;
+
+-- insert
+insert into guestbook values(null, '이원석', '1234', 'ㅎㅇ', now());
+
+-- select
+select no, name, date_format(reg_date, '%Y/%m/%d %H:%i:%s') as reg_date, message from guestbook order by reg_date desc;
+
+-- delete
+delete from guestbook where no=1 and password='1234';
+
